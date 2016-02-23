2dab40d6b29cd5a03c7720194ad2cf17e82c95ec
flarn2006 <flarn2006@gmail.com>
2016-02-22 19:43:28 -0500

    Moved manifest dependency to the project settings

--- RunWithParams/main.c
+++ RunWithParams/main.c
@@ -1,10 +1,6 @@
 #include <Windows.h>
 #include "resource.h"
 
-#pragma comment(linker,"\"/manifestdependency:type='win32' \
-name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
-processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 LPTSTR target;
 int showCmd;
 
